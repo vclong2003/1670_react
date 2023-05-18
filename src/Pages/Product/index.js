5c36c22ce011b515193fe5f95adcbb1f5c018dfa
vclong2003 <vclong2003@gmail.com>
2023-05-18 07:43:26 +0700

    add packages

--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -48,10 +48,6 @@ export default function Product() {
               <ProductItem />
               <ProductItem />
               <ProductItem />
-
-              <div className="col-12">
-                <PaginationBar />
-              </div>
             </div>
           </div>
           {/* Shop Product End */}
@@ -69,40 +65,6 @@ function CategoryItem({ name, id }) {
       to={{ search: `category=${id}` }}>
       {name}
     </Link>
-  );
-}
-
-function PaginationBar() {
-  return (
-    <nav>
-      <ul className="pagination justify-content-center">
-        <li className="page-item disabled">
-          <a className="page-link" href="#">
-            Previous
-          </a>
-        </li>
-        <li className="page-item active">
-          <a className="page-link" href="#">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            3
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            Next
-          </a>
-        </li>
-      </ul>
-    </nav>
   );
 }
 
