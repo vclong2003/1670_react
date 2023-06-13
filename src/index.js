a75d975920d2131b4dba7635c2613d98b920dc25
vclong2003 <vclong2003@gmail.com>
2023-06-13 21:10:29 +0700

    improve performance

--- src/index.js
+++ src/index.js
@@ -50,11 +50,11 @@ function App() {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
